dfc0af1dc5d408674dc10e2e9abf90b55e6260a2
Nickollas <nickollas001@users.noreply.github.com>
2022-06-28 12:11:08 -0300

    cup-campeonato-js

--- js/campeonatos.js
+++ js/campeonatos.js
@@ -1,22 +1,45 @@
 let brawn = document.getElementById("brawn");
 let brawnFoto = document.getElementById("brawn-img");
-
-brawn.addEventListener("mouseover",function(){
-    this.style.height = "310vh"
-    brawnFoto.style.height = "260px"
-})
-brawn.addEventListener("mouseout",function(){
-    this.style.height = ""
-    brawnFoto.style.height = ""
-})
 let cup = document.getElementById("cup");
 let cupFoto = document.getElementById("cup-img");
 
-cup.addEventListener("mouseover",function(){
-    this.style.height = "310px"
-    cupFoto.style.height = "260px"
-})
+const celular = matchMedia ('(min-width:769px)');
+const changeSize = mql => {
+    mql.matches
+    ? cup.addEventListener("mouseover",function(){
+        this.style.height = "310px"
+        cupFoto.style.height = "260px"
+    })
+    : cup.addEventListener("mouseout",function(){
+        this.style.height = ""
+        cupFoto.style.height = "220px"
+    })
+}
+
 cup.addEventListener("mouseout",function(){
     this.style.height = ""
-    cupFoto.style.height = ""
+    cupFoto.style.height = "220px"
 })
+
+const celular2 = matchMedia ('(min-width:769px)');
+const changeSize2 = mql2 => {
+    mql2.matches
+    ? brawn.addEventListener("mouseover",function(){
+        this.style.height = "310px"
+        brawnFoto.style.height = "260px"
+    })
+    : brawnFoto.addEventListener("mouseout",function(){
+        this.style.height = ""
+        brawnFoto.style.height = "220px"
+    })
+}
+
+brawn.addEventListener("mouseout",function(){
+    this.style.height = ""
+    brawnFoto.style.height = "220px"
+})
+
+celular.addListener(changeSize);
+changeSize(celular);
+celular2.addListener(changeSize2);
+changeSize2(celular2);
