4da1662476dd26b0bc91922035b90756c55b6428
Nickollas <nickollas001@users.noreply.github.com>
2022-07-01 13:07:00 -0300

    att

--- js/campeonatos.js
+++ js/campeonatos.js
@@ -9,16 +9,19 @@ const changeSize = mql => {
     ? cup.addEventListener("mouseover",function(){
         this.style.height = "310px"
         cupFoto.style.height = "260px"
+        brawnFoto.style.opacity = "0.4"
     })
     : cup.addEventListener("mouseout",function(){
         this.style.height = ""
         cupFoto.style.height = "220px"
+        brawnFoto.style.opacity = "1"
     })
 }
 
 cup.addEventListener("mouseout",function(){
     this.style.height = ""
     cupFoto.style.height = "220px"
+    brawnFoto.style.opacity = "1"
 })
 
 const celular2 = matchMedia ('(min-width:769px)');
@@ -27,16 +30,20 @@ const changeSize2 = mql2 => {
     ? brawn.addEventListener("mouseover",function(){
         this.style.height = "310px"
         brawnFoto.style.height = "260px"
+        cupFoto.style.opacity = "0.4"
     })
     : brawnFoto.addEventListener("mouseout",function(){
         this.style.height = ""
         brawnFoto.style.height = "220px"
+        cupFoto.style.opacity = "1"
     })
 }
 
 brawn.addEventListener("mouseout",function(){
     this.style.height = ""
     brawnFoto.style.height = "220px"
+    cupFoto.style.opacity = "1"
+
 })
 
 celular.addListener(changeSize);
