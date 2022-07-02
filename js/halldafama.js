30ab65a9402a49c37bdd6b09d922ba665cd8089f
Nickollas <nickollas001@users.noreply.github.com>
2022-07-02 01:37:54 -0300

    hall da fama brawn

--- js/halldafama.js
+++ js/halldafama.js
@@ -1,5 +1,7 @@
 var buttonCup = document.getElementById("button-cup");
 var cup = document.getElementById("cup");
+var brawn = document.getElementById("brawn");
+var buttonBrawn = document.getElementById("button-brawn");
 
 buttonCup.addEventListener("mouseover",function(){
     cup.style.color = "red";
@@ -9,6 +11,17 @@ buttonCup.addEventListener("mouseover",function(){
 
 buttonCup.addEventListener("mouseout",function(){
     cup.style.color = "";
+    this.style.background = ""
+    this.style.border = ""
+})
+buttonBrawn.addEventListener("mouseover",function(){
+    brawn.style.color = "red";
+    this.style.background = "white"
+    this.style.border = "1px solid red"
+})
+
+buttonBrawn.addEventListener("mouseout",function(){
+    brawn.style.color = "";
     this.style.background = ""
     this.style.border = ""
 })
